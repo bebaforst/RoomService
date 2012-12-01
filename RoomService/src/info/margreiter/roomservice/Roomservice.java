ad740fa9a44dcb04b85ce784778227888f024722
Thomas Margreiter <tm@felder.at>
2012-12-01 22:30:47 +0100

    rev0.0003

--- RoomService/src/info/margreiter/roomservice/Roomservice.java
+++ RoomService/src/info/margreiter/roomservice/Roomservice.java
@@ -1,6 +1,9 @@
 package info.margreiter.roomservice;
 
+import info.margreiter.roomservice.logon.LogonCtrl;
+import info.margreiter.vaadin.MainCtrl;
 import info.margreiter.vaadin.RenderingException;
+import info.margreiter.vaadin.TabCtrl;
 import info.margreiter.vaadin.VaadXmlRenderer;
 
 import java.io.IOException;
@@ -20,7 +23,7 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.Window;
 
-public class Roomservice extends Application implements ClickListener {
+public class Roomservice extends Application implements ClickListener,MainCtrl {
 	// alt k     alt m    und F4   werden als shortcuttest verwendet
 	
 	ShortcutListener sl=new ShortcutListener("alt+k",ShortcutAction.KeyCode.K, new int[]{ShortcutAction.ModifierKey.ALT}) {
@@ -38,40 +41,22 @@ public class Roomservice extends Application implements ClickListener {
 		}
 	};
 	private VaadXmlRenderer baseUi;
-	private VaadXmlRenderer subUi=null;
 	
 	
 	@Override
 	public void init() {
-		Window mainWindow = new Window("FerpsProdVaadin");
-		Label label = new Label("Hello VaadinTESTj user und so weiter");
-	
-		mainWindow.addComponent(label);
-		TextField ts=new TextField();
+		Window mainWindow = new Window("RoomService");
 		setMainWindow(mainWindow);
 		try {
-				baseUi=readUI("example2.xml");
-				mainWindow.addComponent(baseUi.getRoot());
-				Button addTab = (Button) baseUi.getById("addTab");
-				addTab.addListener((ClickListener) this);
-				addTab.addShortcutListener(sl);
-				
-				Button shortCutButton=(Button) baseUi.getById("removeTab");
-				shortCutButton.setClickShortcut(KeyCode.F4);
-				shortCutButton.addListener(this);
-				
-				Button mnemonicButton=(Button) baseUi.getById("myBut3");
-				mnemonicButton.addShortcutListener(sl2);
-				
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (RenderingException e) {
+			initSubCtrl(new LogonCtrl(this));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		
-		
+	
+	}
+	public void initSubCtrl(TabCtrl subCtrl){
+			getMainWindow().removeAllComponents();
+			getMainWindow().addComponent(subCtrl.getUi().getRoot());
 	}
 	
 	private VaadXmlRenderer readUI(String resource) throws IOException, RenderingException {
@@ -91,42 +76,9 @@ public class Roomservice extends Application implements ClickListener {
 		System.out.println("caption:" + event.getButton().getCaption() + " .. id: " + event.getButton().getDebugId() + event.getButton() + event.isCtrlKey());
 		System.out.println("button click funktioniert " + event.toString());
 		String caption = event.getButton().getCaption();
-		if ("removeTab".equals(caption)) removeTab();
-		if ("addTab".equals(caption)) addTab();
+//		if ("removeTab".equals(caption)) removeTab();
+//		if ("addTab".equals(caption)) addTab();
 		
 				
 	}
-
-	private void addTab() {
-		// TODO Test 25.01.2012
-		removeTab();
-		try {
-			subUi=readUI("subexample2.xml");
-			Component tabsheet = subUi.getById("removeAbleGrid");
-//			Component mainPanel = baseUi.getById("mainPanel");
-			Component mainPanel = baseUi.getById("mainTab");
-			if (mainPanel instanceof ComponentContainer) {
-				ComponentContainer container = (ComponentContainer)mainPanel;
-				container.addComponent(tabsheet);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (RenderingException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void removeTab() {
-		// TODO Test 25.01.2012
-		if (null!=subUi){
-			Component tabsheet = subUi.getById("removeAbleGrid");			
-			Component mainPanel = baseUi.getById("mainPanel");
-			if (mainPanel instanceof ComponentContainer) {
-				ComponentContainer container = (ComponentContainer)mainPanel;
-				container.removeComponent(tabsheet);
-			}
-			subUi=null;
-		}
-	}
-	
 }
